8f7cdaaa29d69e0d1c549f376c1c76e2b41bc496
Oons <oons.hundred.pixel@gmail.com>
2016-04-12 16:43:09 +0200

    Test one

--- Hi/src/fr/skys/hi/Hi.java
+++ Hi/src/fr/skys/hi/Hi.java
@@ -5,7 +5,8 @@ public class Hi {
 	public static void main(String[] args) {
 		System.out.println("Hi all!");
 		System.out.println("-------");
-
+		System.out.println("Test pour tuto");
+		System.out.println("Yop");
 	}
 
 }
